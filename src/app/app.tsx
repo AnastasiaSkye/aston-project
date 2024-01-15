ec5045b44e52e6c2c5b1532a213c0f302c15d6bf
AnastasiaSkye <juliadeiker@yandex.ru>
2024-01-16 02:06:42 +0300

    add prettier and eslint

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Main } from '../pages/main/main';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Main />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,7 +4,6 @@ const rootReducer = combineReducers({});
 
 export const store = configureStore({
     reducer: rootReducer
-
 })
 
 export type RootState = ReturnType<typeof store.getState>
