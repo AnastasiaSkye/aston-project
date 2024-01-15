707b7ef4beeb3e2b14320d88121178ba267ab1ae
AnastasiaSkye <juliadeiker@yandex.ru>
2024-01-16 01:36:50 +0300

    add redux toolkit and eact router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { Main } from '../pages/main/main';
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
